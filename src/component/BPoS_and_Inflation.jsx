156b216142a7d9ea142ef624e66e6648de526ef5
Chhotelal Patel <chhotupatel705@gmail.com>
2025-03-11 08:25:43 +0530

    update

---
+++ src/component/BPoS_and_Inflation.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const BPoS_and_Inflation = () => {
+  return (
+    <div>BPoS_and_Inflation</div>
+  )
+}
--- src/page/TanHomePage.jsx
+++ src/page/TanHomePage.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import {HeroSection} from "../component/HeroSection";
+import { HeroSection } from "../component/HeroSection";
+import { BPoS_and_Inflation } from "../component/BPoS_and_Inflation";
 
- export const TanHomePage = () => {
-  return <HeroSection />;
+export const TanHomePage = () => {
+  return (
+    <>
+      <HeroSection />
+      <BPoS_and_Inflation/>
+
+    </>
+  );
 };
-
-
