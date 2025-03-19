f38f209c328555528342f4f53ce6549712f745e1
Chhotelal Patel <chhotupatel705@gmail.com>
2025-03-19 08:03:24 +0530

    update

--- src/component/BPoS_and_Inflation.jsx
+++ src/component/BPoS_and_Inflation.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import {Box, Stack, Typography} from '@mui/material';
+import Grid from '@mui/material/Grid2';
 
 export const BPoS_and_Inflation = () => {
   return (
-    <div>BPoS_and_Inflation</div>
-  )
-}
+    <Box sx={{ flexGrow: 1, paddingX:18, backgroundColor: 'black' }}>
+      <Grid container spacing={2} sx={{ backgroundColor: 'blue' }}>
+        <Grid size={{ xs: 7 }} >
+          <Stack spacing={2}>
+            <Typography sx={{ color: 'white', backgroundColor: 'red' }}>Column 1 - Row 1</Typography>
+            <Typography sx={{ color: 'white', backgroundColor: 'red' }}>Column 1 - Row 2</Typography>
+            <Typography sx={{ color: 'white', backgroundColor: 'red' }}>Column 1 - Row 3</Typography>
+          </Stack>
+        </Grid>
+        <Grid  size={{ xs: 5 }}>
+          <Typography sx={{ color: 'white', backgroundColor: 'red', height: '100%', boxSizing: 'border-box' }}>
+            Column 2
+          </Typography>  
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
